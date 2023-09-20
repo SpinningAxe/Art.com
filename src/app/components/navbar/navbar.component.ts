fe202a07ac9bdea681ade3e89a68147cfe125861
Vo Van Viet <vovanviet4444@gmail.com>
2023-09-21 01:16:02 +0700

    80% UI

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 interface pageInfo{
@@ -13,6 +13,7 @@ interface pageInfo{
 })
 export class NavbarComponent {
 
+  @Input() title!:string
   constructor(public route: Router) {  }
   
   pageList:pageInfo[] = [
--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [
         NavbarComponent
+    ],
+    imports:[
+        CommonModule
     ],
     exports: [
         NavbarComponent
